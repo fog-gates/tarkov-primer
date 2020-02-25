d030691ef35adad425abedaa5a84c88386d55a27
Forest Gates <fog_gates@yahoo.com>
2020-02-25 17:57:05 -0500

    Barebones functionality added

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$('#info').one("click", function() {
+    $('.form').animate({'marginTop':"-=25%"});
+    return false;
+});
+$('#customs').click( function() {
+    $('img').fadeOut();
+    $('#customsMap').fadeIn();
+    return false;
+});
+$('#shoreline').click( function() {
+    $('img').fadeOut();
+    $('#shorelineMap').fadeIn();
+    return false;
+});
+$('#interchange').click( function() {
+    $('img').fadeOut();
+    $('#interchangeMap').fadeIn();
+    return false;
+});
+$('#factory').click( function() {
+    $('img').fadeOut();
+    $('#factoryMap').fadeIn();
+    return false;
+});
+$('#labs').click( function() {
+    $('img').fadeOut();
+    $('#labsMap').fadeIn();
+    return false;
+});
+$('#reserve').click( function() {
+    $('img').fadeOut();
+    $('#reserveMap').fadeIn();
+    return false;
+});
+$('#ammo').click( function() {
+    $('img').fadeOut();
+    $('#ammoMap').fadeIn();
+    return false;
+});
